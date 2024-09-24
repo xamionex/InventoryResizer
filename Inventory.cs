ecb9273461ddbb803d9f7f5c12f00296efd7e08d
amione <57235791+xamionex@users.noreply.github.com>
2024-09-24 13:26:08 +0200

    first commit

---
+++ Inventory.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using InventoryResizer;
+using UnityEngine;
+
+[HarmonyPatch]
+public class InventoryPatch
+{
+	[HarmonyPatch(typeof(Inventory), nameof(Inventory.show))]
+	[HarmonyTranspiler]
+	public static IEnumerable<CodeInstruction> InventoryShow(IEnumerable<CodeInstruction> instructions)
+	{
+		var codes = new List<CodeInstruction>(instructions);
+		for (int i = 0; i < codes.Count; i++)
+		{
+			// replaces 670 offset for workbench crafting with 1000
+			if (codes[i].opcode == OpCodes.Ldc_R4 && codes[i].operand.ToString() == "670")
+			{
+				codes[i] = new CodeInstruction(OpCodes.Ldc_R4, 1000f);
+				break;
+			}
+		}
+		return codes;
+	}
+
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.show))]
+    [HarmonyPrefix]
+    public static void AddSlots(Inventory __instance, string labelName = "")
+    {
+        if (Plugin.InventorySlots.Value && __instance.invType == Inventory.InvType.playerInv && __instance.slots.Count != Plugin.InventoryRightSlots.Value*Plugin.InventoryDownSlots.Value)
+        {
+			__instance.maxColumns = Plugin.InventoryRightSlots.Value;
+			ModifySlots(__instance, __instance.maxColumns*Plugin.InventoryDownSlots.Value);
+        }
+        if (labelName == "Storage" && __instance.slots.Count != Plugin.RightSlots.Value*Plugin.DownSlots.Value)
+        {
+			__instance.maxColumns = Plugin.RightSlots.Value;
+			ModifySlots(__instance, __instance.maxColumns*Plugin.DownSlots.Value);
+        }
+    }
+
+	public static void ModifySlots(Inventory __instance, int MaximumSlots)
+	{
+		Plugin.Log.LogInfo($"Maximum slots allowed is {MaximumSlots} and we have {__instance.slots.Count}");
+		if (MaximumSlots < __instance.slots.Count && Plugin.RemoveExcess.Value)
+		{
+			Plugin.Log.LogInfo($"Removing {__instance.slots.Count - MaximumSlots} slots");
+			for (int i = __instance.slots.Count; i > MaximumSlots; i--)
+			{
+				__instance.slots.RemoveAt(i-1);
+			}
+		} else {
+			Plugin.Log.LogInfo($"Adding {MaximumSlots - __instance.slots.Count} slots");
+			for (int i = __instance.slots.Count; i < MaximumSlots; i++)
+			{
+				__instance.slots.Add(new InvSlot());
+			}
+		}
+		return;
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+
+namespace InventoryResizer;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+[BepInProcess("Darkwood.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static PluginInfo pluginInfo;
+    public static ConfigFile ConfigFile;
+    public const string PluginGUID = PluginAuthor + "." + PluginName;
+    public const string PluginAuthor = "amione";
+    public const string PluginName = "InventoryResizer";
+    public const string PluginVersion = "1.0.0";
+    public static ManualLogSource Log;
+    public static FileSystemWatcher fileWatcher;
+    public static ConfigEntry<int> RightSlots;
+    public static ConfigEntry<int> DownSlots;
+    public static ConfigEntry<int> InventoryRightSlots;
+    public static ConfigEntry<int> InventoryDownSlots;
+    public static ConfigEntry<bool> InventorySlots;
+    public static ConfigEntry<bool> RemoveExcess;
+
+    private void Awake()
+    {
+        pluginInfo = Info;
+        Log = Logger;
+        ConfigFile = new ConfigFile(System.IO.Path.Combine(Paths.ConfigPath, PluginGUID + ".cfg"), true);
+
+        RightSlots = ConfigFile.Bind($"{PluginName}", "Storage Right Slots", 12, "Number that determines slots in workbench to the right");
+        DownSlots = ConfigFile.Bind($"{PluginName}", "Storage Down Slots", 9, "Number that determines slots in workbench downward");
+        InventoryRightSlots = ConfigFile.Bind($"{PluginName}", "Inventory Right Slots", 2, "Number that determines slots in inventory to the right");
+        InventoryDownSlots = ConfigFile.Bind($"{PluginName}", "Inventory Down Slots", 9, "Number that determines slots in inventory downward");
+        InventorySlots = ConfigFile.Bind($"{PluginName}", "Enable Inventory Changing", false, "This will circumvent the inventory progression and enables the 2 settings above this one to take effect");
+        RemoveExcess = ConfigFile.Bind($"{PluginName}", "Remove Excess Slots", true, "Whether or not to remove slots that are outside the inventory you set. For example, you set your inventory to 9x9 (81 slots) but you had a previous mod do something bigger and you have something like 128 slots extra enabling this option will remove those excess slots and bring it down to 9x9 (81)");
+
+        Harmony Harmony = new Harmony($"{PluginGUID}");
+        Harmony.PatchAll(typeof(InventoryPatch));
+        Log.LogInfo($"Plugin {PluginGUID} v{PluginVersion} is loaded!");
+
+        fileWatcher = new FileSystemWatcher(Paths.ConfigPath, PluginGUID + ".cfg");
+        fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+        fileWatcher.Changed += OnFileChanged;
+        fileWatcher.EnableRaisingEvents = true;
+    }
+    private void OnFileChanged(object sender, FileSystemEventArgs e)
+    {
+        ConfigFile.Reload();
+        Log.LogInfo($"Reloaded configuration file");
+    }
+}
